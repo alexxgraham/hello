5f3e21fa927d8fad1703625045c0ac64e1443aa4
alexxgraham <destgraham@icloud.com>
2025-05-31 08:31:53 -0700

    refreshed site

--- app/db.server.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-import { singleton } from "./singleton.server";
-
-const prisma = singleton("prisma", () => new PrismaClient());
-prisma.$connect();
-
-export { prisma };
